9e9042099e6878830173e737ce519a6602a190e7
John Mackerras <github@desmac.com.au>
2026-02-06 15:33:55 +0000

    InitialCreate

---
+++ README.md
@@ -0,0 +1,2 @@
+# SoftwareDevelopmentManager
+A tool to help me manage all of the sofwtare development projects I have in Git
---
+++ SolutionManager/Models/SolutionManagerOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace SolutionManager.Models
+{
+    public sealed class SolutionManagerOptions
+    {
+        /// <summary>
+        /// Where all repos live locally (e.g. C:\Development\).
+        /// </summary>
+        public string DevRootPath { get; set; } = default;
+
+        /// <summary>
+        /// Default remote repo URL root (used if you only store repo name).
+        /// Example: https://github.com/johnmackerras/
+        /// </summary>
+        public string DefaultRemoteRepoRootUrl { get; set; } =  default;
+    }
+}
